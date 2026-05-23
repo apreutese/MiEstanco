343aa09aaca57b1f20e015896ff4dc4fe85f446c
apreutese <leonardap22@gmail.com>
2026-05-23 22:29:11 +0200

    feat: PWA (manifest+service worker+iconos), historial layout 2 lineas movil

--- miestanco-frontend/src/app/app.config.ts
+++ miestanco-frontend/src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { routes } from './app.routes';
 import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,7 +19,11 @@ export const appConfig: ApplicationConfig = {
         duration: 2500,
         horizontalPosition: 'center',
         verticalPosition: 'bottom',
-      }
-    }
-  ]
+      },
+    },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 };
