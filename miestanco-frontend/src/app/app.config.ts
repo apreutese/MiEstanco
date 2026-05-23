e02f034775ac184e5d54ba0eebadcfa53b38c27b
apreutese <leonardap22@gmail.com>
2026-05-23 21:46:31 +0200

    fix: themes light (blanco base, Marlboro Red #CC0000, Gold #C9973A, Tabacos burdeos), selects nativos, tablas scroll movil, snackbar centrado

--- miestanco-frontend/src/app/app.config.ts
+++ miestanco-frontend/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { routes } from './app.routes';
 import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
 
@@ -11,5 +12,13 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptors([jwtInterceptor])),
     provideAnimationsAsync(),
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+      useValue: {
+        duration: 2500,
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+      }
+    }
   ]
 };
